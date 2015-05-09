fb4496e36b3baa589ed8fa68ca2a1af5b9248ebe
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-05-09 17:46:44 +0100

    Fix bug in db/seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,13 @@ hostel_ids = Hostel.all.map {|hostel| hostel.id }
 #we're assuming that each student has an acomodation
 
 students.each do |student|
-  accomodation = Accomodation.create!(validity_period: "One Session (2014/2015)", has_occupant: 1, student_id: student.id, hostel_id: rand( 1..hostel_ids.length ))
-  accomodation.update_attribute("room_nos",  { rand(1..accomodation.hostel.room_count).to_s.rjust(3, '0')})
+  accomodation = Accomodation.create!(
+    validity_period: "One Session (2014/2015)",
+    has_occupant: 1,
+    student_id: student.id,
+    hostel_id: rand( 1..hostel_ids.length )
+  )
+
+  accomodation.update_attribute("room_nos", rand(1..accomodation.hostel.room_count).to_s.rjust(3, '0'))
   accomodation.save!
 end
