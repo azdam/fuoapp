042a080444dda4d11837128216ab97c01e4cc8b7
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-06-16 15:00:26 +0100

    Seed Admin data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 require 'date'
 
+Student.delete_all
 #create 20 students
 20.times { FactoryGirl.create :student }
 
+Hostel.delete_all
 #create 5 hostels
 5.times do |n|
   Hostel.create!( name: "FUO Hostel #{n + 1}", description: "This is FUO Hostel #{n + 1}, created on #{Date.new(rand(2005..2013), 2, 3)}", room_count: rand(90..150) )
@@ -10,6 +12,7 @@ end
 
 #crate accomodations
 
+Accomodation.delete_all
 students = Student.all
 hostel_ids = Hostel.all.map {|hostel| hostel.id }
 #we're assuming that each student has an acomodation
@@ -25,3 +28,6 @@ students.each do |student|
   accomodation.update_attribute("room_nos", rand(1..accomodation.hostel.room_count).to_s.rjust(3, '0'))
   accomodation.save!
 end
+
+#create Administrator
+Administrator.create(email: "azeezat@fuo.edu.ng", password: "password123")
