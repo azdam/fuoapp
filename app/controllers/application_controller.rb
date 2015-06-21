cf483919f27f9370dfe1a46ce36700d241124a05
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-06-21 14:24:42 +0100

    Fix json authentication bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :current_admin
+  before_action :current_admin 
 
   def current_admin
-    if session[:user_id]
-      @current_admin = Administrator.find_by(id: session[:user_id])
-    else
-      redirect_to signin_url,  notice: "Please sgin in with your credentials"
+    if request.format.html?
+      if session[:user_id]
+        @current_admin = Administrator.find_by(id: session[:user_id])
+      else
+        redirect_to signin_url,  notice: "Please sgin in with your credentials"
+      end
     end
     
   end
